09aa929bb916d08c94b9651573d2f6933a19bb7f
justinhwbhang <117044443+justinhwbhang@users.noreply.github.com>
2023-02-06 03:53:45 -0800

    SNW

---
+++ controllers/thought-controller.js
@@ -0,0 +1,62 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  addThought({ params, body }, res) {
+    console.log(params);
+    Thought.create(body)
+      .then(dbUserData => {
+        console.log(dbUserData);
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.ThoughtId },
+      { $push: { replies: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  removeThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.ThoughtId })
+      .then(deletedThought => {
+        if (!deletedThought) {
+          return res.status(404).json({ message: 'No Thought with this id!' });
+        }
+        return User.findOneAndUpdate(
+          { _id: params.UserId },
+          { $pull: { Thoughts: params.ThoughtId } },
+          { new: true }
+        );
+      })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.ThoughtId },
+      { $pull: { replies: { ReactionId: params.ReactionId } } },
+      { new: true }
+    )
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  }
+};
+
+module.exports = thoughtController;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../../controllers/User-controller');
+
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
